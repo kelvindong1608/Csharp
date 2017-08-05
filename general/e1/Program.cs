cad988cb9003b9bcb80d68ba282a3a9f961fb08c
kelvindong1608 <hoangan_kos@yahoo.com>
2017-08-06 11:27:21 +1200

    fist e1

---
+++ general/e1/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {    int s = 10;
+             const int M=100;
+             System.Console.WriteLine("i have to get NZ citizenship");
+            Console.WriteLine(" your score {0}+ {1}/{2}",M,s, s+M);
+           Console.ReadLine();
+        }
+    }
+}
---
+++ general/e2_if/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e2_if
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            Console.WriteLine(" pls input your age: \n");
+            string a1 = Console.ReadLine();
+            age = System.Convert.ToInt32(a1);
+            /* age =Int32.Parse(a1);*/
+            if (age < 0)
+                Console.WriteLine(" ur data was wrong");
+            else if (age < 18)
+                Console.WriteLine("u r young");
+            else
+                Console.WriteLine("u r old");
+            Console.ReadLine();
+
+
+      
+        }
+    }
+}
