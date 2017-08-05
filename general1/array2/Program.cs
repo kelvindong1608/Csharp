5ae3d5bdec28021e098d0ae7b3bff35426e207bf
kelvindong1608 <hoangan_kos@yahoo.com>
2017-08-06 11:29:36 +1200

    e2

---
+++ general1/array2/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace array2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            DateTime b = DateTime.Now;
+            int[,] a =
+                {
+                { 3,6,2,5},
+                {0,4,5,2 },
+                {3,6,1,3 }
+            };
+            for (int i=0; i<3;i++)
+            {
+                for (int j = 0; j < 4; j++)
+                    Console.WriteLine("Element a[{0}][{1}] = {2}", i, j, a[i, j]);
+                Console.WriteLine("---------");
+            
+            }
+            Console.WriteLine(" now is {0}", b);
+            Console.WriteLine(" sqare root of 35 is {0}", Math.Round(Math.Sqrt(35), 2));
+            Console.WriteLine(" sqare root of 35 is {0}",Math.Sqrt(35));
+            Console.ReadLine();
+        }
+    }
+}
---
+++ general1/e3_array/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace e3_array
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] a = { 12,13,14,15};
+            Console.WriteLine("element count of array {0}: ", a.Length);
+            for(int i=0; i< a.Length; i++)
+            {
+                Console.WriteLine(" Element at {0} is {1}", i, a[i]);
+            }
+            Console.ReadLine();
+        }
+    }
+}
